702b4b278b9496a45b3a53bde8c5de507681651b
Emmanuel TRAN <emmanuel.tran@finopsia.com>
2024-03-18 10:32:32 +0100

    plouf debut

---
+++ Python 00 - Starting/ex00/Hello.py
@@ -0,0 +1,19 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+# LIST
+ft_list[1] = 'World!'
+# TUPLE
+ft_tuple = ("Hello", "France!")
+# SET
+ft_set.discard("tutu!")
+ft_set.add("Paris!")
+# DICT
+ft_dict["Hello"] = "42Paris!" 
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
